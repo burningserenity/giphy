e408a4658e46f2c50d6c97d2d0f0386115eb9f0d
burningserenity <burningserenity@bitmai.la>
2017-07-14 11:45:19 -0400

    Added list of topics

---
+++ assets/javascript/query.js
@@ -0,0 +1,3 @@
+var topics['The Legend of Zelda', 'Earthbound', 'Deus Ex', 'Hotline Miami',
+'Morrowind', 'Wolfenstein: The New Order', 'HITMAN', 'Metroid Prime',
+'Metal Gear Solid', 'Resident Evil'];
